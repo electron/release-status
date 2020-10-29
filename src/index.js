8662100b108e3d9b02dabd3a2a60a0ae3fd57405
Samuel Attard <samuel.r.attard@gmail.com>
2020-10-28 23:47:46 -0700

    feat: add main releases landing page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const fetch = require('node-fetch');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const Handlebars = require('handlebars');
+const fetch = require("node-fetch");
+const path = require("path");
+const semver = require('semver');
 
 const app = express();
 
@@ -12,38 +14,76 @@ let releases = {
 
 const getReleasesOrUpdate = async () => {
   if (Date.now() - releases.lastFetch > 60 * 1000) {
-    const response = await fetch.default('https://electronjs.org/headers/index.json');
+    const response = await fetch.default(
+      "https://electronjs.org/headers/index.json"
+    );
     releases = {
       lastFetch: Date.now(),
       data: await response.json(),
-    }
+    };
   }
   return releases.data;
+};
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "views"));
+
+if (process.env.NODE_ENV === "production") {
+  app.enable("view cache");
 }
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-app.set('views', path.resolve(__dirname, 'views'));
-
-if (process.env.NODE_ENV === 'production') {
-  app.enable('view cache');
-}
-
-app.get('/releases.json', (req, res) => {
-  getReleasesOrUpdate().then((data) => res.json(data)).catch((err) => res.status(500).json({ error: true }));
+app.get("/releases.json", (req, res) => {
+  getReleasesOrUpdate()
+    .then((data) => res.json(data))
+    .catch(() => res.status(500).json({ error: true }));
 });
 
-app.get('/', (req, res) => res.render('home'));
-app.get('/history', (req, res) => res.render('history'));
+Handlebars.registerHelper("timeSince", function(str) {
+  const d = new Date();
+  const today = new Date(d.getFullYear(), d.getMonth() + 1, d.getDate());
+  const parts = str.split('-').map(n => parseInt(n, 10));
+  const releaseDate = new Date(parts[0], parts[1], parts[2]);
+  const daysAgo = Math.floor((today.getTime() - releaseDate.getTime()) / (1000 * 60 * 60 * 24));
+  if (daysAgo === 0) return 'Today';
+  if (daysAgo === 1) return 'Yesterday';
+  return `${daysAgo} days ago`;
+});
 
-app.use(express.static(path.resolve(__dirname, 'static'), {
-  fallthrough: true,
-}));
+app.get("/", (req, res) =>
+  getReleasesOrUpdate()
+    .then((releases) => {
+      releases.sort((a, b) => semver.compare(b.version, a.version));
+      const lastNightly = releases.find(r => semver.parse(r.version).prerelease[0] === 'nightly');
+      const lastBeta = releases.find(r => semver.parse(r.version).prerelease[0] === 'beta');
+      const betaMajor = semver.parse(lastBeta.version).major;
+      const latestSupported = [betaMajor - 1, betaMajor - 2, betaMajor - 3].map(major => releases.find(r => semver.parse(r.version).major === major));
+      res.render("home", {
+        releases,
+        lastNightly,
+        lastBeta,
+        latestSupported,
+        css: 'home',
+      })
+    })
+    .catch(() => res.status(500).json({ error: true }))
+);
+app.get("/history", (req, res) =>
+  res.render("history", {
+    css: "history",
+  })
+);
+
+app.use(
+  express.static(path.resolve(__dirname, "static"), {
+    fallthrough: true,
+  })
+);
 
 app.use((req, res) => {
-  res.redirect('/');
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('Electron release history listening');
+  console.log("Electron release history listening");
 });
