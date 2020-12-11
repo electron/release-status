8036bc7f907e665c9b1cb4a29524cb6a00c2d98f
Samuel Attard <samuel.r.attard@gmail.com>
2020-12-11 13:52:59 -0800

    feat: show currently releasing things

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ let releases = {
   data: null,
 };
 
+let activeReleases = {
+  lastFetch: 0,
+  data: null,
+}
+
 const getReleasesOrUpdate = async () => {
   if (Date.now() - releases.lastFetch > 60 * 1000) {
     const response = await fetch.default('https://electronjs.org/headers/index.json');
@@ -21,6 +26,17 @@ const getReleasesOrUpdate = async () => {
     };
   }
   return releases.data;
+};
+
+const getActiveReleasesOrUpdate = async () => {
+  if (Date.now() - activeReleases.lastFetch > 30 * 1000) {
+    const response = await fetch.default('https://electron-sudowoodo.herokuapp.com/active');
+    activeReleases = {
+      lastFetch: Date.now(),
+      data: await response.json(),
+    };
+  }
+  return activeReleases.data;
 };
 
 app.engine('handlebars', exphbs());
@@ -45,6 +61,12 @@ app.get('/releases.json', (req, res) => {
     .catch(() => res.status(500).json({ error: true }));
 });
 
+app.get('/active.json', (req, res) => {
+  getActiveReleasesOrUpdate()
+    .then((data) => res.json(data))
+    .catch(() => res.status(500).json({ error: true }));
+});
+
 const timeSince = (str) => {
   const d = new Date();
   const today = new Date(d.getFullYear(), d.getMonth() + 1, d.getDate());
@@ -55,6 +77,18 @@ const timeSince = (str) => {
   if (daysAgo === 1) return 'Yesterday';
   return `${daysAgo} days ago`;
 };
+
+const minutesSince = (str) => {
+  const d = new Date(str);
+  const now = new Date();
+  const since = now.getTime() - d.getTime();
+  let minutes = Math.floor(since / 60000);
+  if (minutes < 60) return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+  const hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  if (minutes === 0) return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
+  return `${hours} hour${hours !== 1 ? 's' : ''} and ${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+}
 
 Handlebars.registerPartial('releaseBar', function (version) {
   return `<a class="release" href="https://github.com/electron/electron/releases/tag/v${
@@ -77,9 +111,32 @@ Handlebars.registerPartial('releaseBar', function (version) {
 </a>`;
 });
 
+Handlebars.registerPartial('releaseSquare', function (release) {
+  let channelIcon = '<i class="fas fa-pencil-alt"></i>';
+  switch (release.channel) {
+    case 'minor': {
+      channelIcon = '<i class="fas fa-feather"></i>';
+      break;
+    };
+    case 'beta': {
+      channelIcon = '<i class="fas fa-flask"></i>';
+      break;
+    };
+    case 'nightly': {
+      channelIcon = '<i class="fas fa-moon"></i>';
+      break;
+    }
+  }
+  return `<div class="active-release">
+  <span><i class="fas fa-code-branch"></i>${release.branch}</span>
+  <span>${channelIcon}${release.channel.substr(0, 1).toUpperCase()}${release.channel.substr(1)} Release</span>
+  <span>Started ${minutesSince(release.started)}</span>
+</div>`;
+});
+
 app.get('/', (req, res) =>
-  getReleasesOrUpdate()
-    .then((releases) => {
+  Promise.all([getReleasesOrUpdate(), getActiveReleasesOrUpdate()])
+    .then(([releases, activeReleases]) => {
       releases.sort((a, b) => semver.compare(b.version, a.version));
       const lastNightly = releases.find((r) => semver.parse(r.version).prerelease[0] === 'nightly');
       const lastBeta = releases.find((r) => semver.parse(r.version).prerelease[0] === 'beta');
@@ -92,6 +149,8 @@ app.get('/', (req, res) =>
         lastNightly,
         lastBeta,
         latestSupported,
+        currentlyReleasing: activeReleases.currentlyRunning,
+        queuedReleases: activeReleases.queued,
         css: 'home',
       });
     })
