e5923b452962d51454a18b767dcc63afbf20ce55
Samuel Attard <samuel.r.attard@gmail.com>
2021-07-30 13:24:03 -0700

    feat: support commit range syntax and show change in dep versions

--- src/routes/release.js
+++ src/routes/release.js
@@ -35,15 +35,16 @@ const knownSections = [
 
 Handlebars.registerHelper('markdownMergeHeaders', function (contentArr) {
   const groups = {};
+  const hasPrerelease = contentArr.find((c) => c[1].includes('-'));
   for (const [content, version] of contentArr) {
     const headers = content.split(/^## ([A-Za-z ]+)\n/gm).slice(1);
     for (let i = 0; i < headers.length; i += 2) {
       const groupName = headers[i];
       const groupContent = headers[i + 1];
       groups[groupName] = groups[groupName] || '';
-      groups[
-        groupName
-      ] += `<div class="notes-version-group"><div>${version}</div>\n\n${groupContent.trim()}\n\n</div>\n`;
+      groups[groupName] += `<div class="notes-version-group ${
+        hasPrerelease ? 'maybe-prerelease' : ''
+      }"><div>${version}</div>\n\n${groupContent.trim()}\n\n</div>\n`;
     }
   }
   let generatedNotes = '# Combined Release Notes\n\n';
@@ -55,60 +56,77 @@ Handlebars.registerHelper('markdownMergeHeaders', function (contentArr) {
   return DOMPurify.sanitize(md.render(generatedNotes));
 });
 
+async function handleComparisonRequest(startVersion, endVersion, res) {
+  const allReleases = await getReleasesOrUpdate();
+
+  const parsedStart = semver.parse(startVersion);
+  const parsedEnd = semver.parse(endVersion);
+  if (parsedStart.major !== parsedEnd.major) {
+    return res.end(
+      'Sorry you can only compare Electron versions in the same major line at the moment',
+    );
+  }
+
+  if (semver.gt(startVersion, endVersion)) {
+    res.redirect(`/release/compare/${endVersion}/${startVersion}`);
+  }
+
+  // Bad start === give up
+  if (!allReleases.find((r) => r.version === startVersion.substr(1))) {
+    return res.redirect('/');
+  }
+
+  // Bad end === give up
+  if (!allReleases.find((r) => r.version === endVersion.substr(1))) {
+    return res.redirect('/');
+  }
+
+  const versionRange = allReleases.filter(
+    (r) =>
+      semver.parse(r.version).compare(startVersion) > 0 &&
+      semver.parse(r.version).compare(endVersion) <= 0,
+  );
+  const allGitHubReleases = await Promise.all(
+    versionRange.map((r) => getGitHubRelease(`v${r.version}`)),
+  );
+  const allNotes = allGitHubReleases.filter(Boolean).map((r) => {
+    let notes = r.body;
+    const parsed = semver.parse(r.tag_name);
+    if (parsed.prerelease.length) {
+      notes = notes.split(`@${r.tag_name.substr(1)}\`.`)[1];
+    }
+    notes = notes.replace(`Release Notes for ${r.tag_name}`, 'Release Notes');
+    return [notes, r.tag_name];
+  });
+
+  const startReleaseMeta = allReleases.find((r) => r.version === startVersion.substr(1));
+  const endReleaseMeta = allReleases.find((r) => r.version === endVersion.substr(1));
+
+  res.render('release-diff', {
+    title: `${startVersion} .. ${endVersion}`,
+    startVersion,
+    endVersion,
+    startReleaseMeta,
+    endReleaseMeta,
+    allNotes,
+    css: 'release',
+  });
+}
+
 router.get(
   '/compare/:startVersion/:endVersion',
   a(async (req, res) => {
     const startVersion = req.params.startVersion;
     const endVersion = req.params.endVersion;
-    const allReleases = await getReleasesOrUpdate();
+    return await handleComparisonRequest(startVersion, endVersion, res);
+  }),
+);
 
-    const parsedStart = semver.parse(startVersion);
-    const parsedEnd = semver.parse(endVersion);
-    if (parsedStart.major !== parsedEnd.major) {
-      return res.end(
-        'Sorry you can only compare Electron versions in the same major line at the moment',
-      );
-    }
-
-    if (semver.gt(startVersion, endVersion)) {
-      res.redirect(`/release/compare/${endVersion}/${startVersion}`);
-    }
-
-    // Bad start === give up
-    if (!allReleases.find((r) => r.version === startVersion.substr(1))) {
-      return res.redirect('/');
-    }
-
-    // Bad end === give up
-    if (!allReleases.find((r) => r.version === endVersion.substr(1))) {
-      return res.redirect('/');
-    }
-
-    const versionRange = allReleases.filter(
-      (r) =>
-        semver.parse(r.version).compare(startVersion) > 0 &&
-        semver.parse(r.version).compare(endVersion) <= 0,
-    );
-    const allGitHubReleases = await Promise.all(
-      versionRange.map((r) => getGitHubRelease(`v${r.version}`)),
-    );
-    const allNotes = allGitHubReleases.map((r) => {
-      let notes = r.body;
-      const parsed = semver.parse(r.tag_name);
-      if (parsed.prerelease.length) {
-        notes = notes.split(`@${r.tag_name.substr(1)}\`.`)[1];
-      }
-      notes = notes.replace(`Release Notes for ${r.tag_name}`, 'Release Notes');
-      return [notes, r.tag_name];
-    });
-
-    res.render('release-diff', {
-      title: `${startVersion} .. ${endVersion}`,
-      startVersion,
-      endVersion,
-      allNotes,
-      css: 'release',
-    });
+router.get(
+  '/compare/:comparisonRange',
+  a(async (req, res) => {
+    const [startVersion, endVersion] = req.params.comparisonRange.split('..');
+    return await handleComparisonRequest(startVersion, endVersion, res);
   }),
 );
 
