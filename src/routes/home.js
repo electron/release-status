6c89fe7ab2003ed760dce07bedc6025568e8bf5b
Erick Zhao <erick@hotmail.ca>
2022-07-26 17:29:54 -0700

    share time format utils

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,33 +4,9 @@ const semver = require('semver');
 
 const a = require('../utils/a');
 const { getReleasesOrUpdate, getActiveReleasesOrUpdate } = require('../data');
+const { timeSince, minutesSince } = require('../utils/format-time');
 
 const router = new Router();
-
-const timeSince = (str) => {
-  const d = new Date();
-  const today = new Date(d.getFullYear(), d.getMonth() + 1, d.getDate());
-  const parts = str.split('-').map((n) => parseInt(n, 10));
-  const releaseDate = new Date(parts[0], parts[1], parts[2]);
-  const daysAgo = Math.floor((today.getTime() - releaseDate.getTime()) / (1000 * 60 * 60 * 24));
-  if (daysAgo === 0) return 'Today';
-  if (daysAgo === 1) return 'Yesterday';
-  return `${daysAgo} days ago`;
-};
-
-const minutesSince = (str) => {
-  const d = new Date(str);
-  const now = new Date();
-  const since = now.getTime() - d.getTime();
-  let minutes = Math.floor(since / 60000);
-  if (minutes < 60) return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
-  const hours = Math.floor(minutes / 60);
-  minutes = minutes % 60;
-  if (minutes === 0) return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-  return `${hours} hour${hours !== 1 ? 's' : ''} and ${minutes} minute${
-    minutes !== 1 ? 's' : ''
-  } ago`;
-};
 
 Handlebars.registerPartial('releaseBar', function (version) {
   return `<a class="release" href="/release/v${version.version}">
@@ -83,7 +59,11 @@ router.get(
       getActiveReleasesOrUpdate(),
     ]);
     const lastNightly = releases.find((r) => semver.parse(r.version).prerelease[0] === 'nightly');
-    const lastPreRelease = releases.find((r) => semver.parse(r.version).prerelease[0] === 'beta' || semver.parse(r.version).prerelease[0] === 'alpha');
+    const lastPreRelease = releases.find(
+      (r) =>
+        semver.parse(r.version).prerelease[0] === 'beta' ||
+        semver.parse(r.version).prerelease[0] === 'alpha',
+    );
     const betaMajor = semver.parse(lastPreRelease.version).major;
     const latestSupported = [betaMajor - 1, betaMajor - 2, betaMajor - 3].map((major) =>
       releases.find((r) => semver.parse(r.version).major === major),
--- src/routes/releases.js
+++ src/routes/releases.js
@@ -7,16 +7,7 @@ const semver = require('semver');
 
 const router = new Router();
 
-const timeSince = (str) => {
-  const d = new Date();
-  const today = new Date(d.getFullYear(), d.getMonth() + 1, d.getDate());
-  const parts = str.split('-').map((n) => parseInt(n, 10));
-  const releaseDate = new Date(parts[0], parts[1], parts[2]);
-  const daysAgo = Math.floor((today.getTime() - releaseDate.getTime()) / (1000 * 60 * 60 * 24));
-  if (daysAgo === 0) return 'Today';
-  if (daysAgo === 1) return 'Yesterday';
-  return `${daysAgo} days ago`;
-};
+const { timeSince } = require('../utils/format-time');
 
 Handlebars.registerPartial('metadata', function (version) {
   return `
