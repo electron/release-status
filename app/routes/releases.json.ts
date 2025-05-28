033862c9726c974f1c8bf32210241d6ab64f11f6
sarthakKarode <sarthakkarode@gmail.com>
2025-05-28 11:06:21 +0530

    CORS #178

---
+++ app/routes/releases.json.ts
@@ -0,0 +1,25 @@
+import { json } from '@remix-run/node';
+import { getReleasesOrUpdate } from '../data/release-data';
+
+export const loader = async ({ request }) => {
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  }
+
+  const releases = await getReleasesOrUpdate();
+
+  return json(releases, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
+};
